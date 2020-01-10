0b0677ca8cbd2df066188c1eae7a6c795d98207d
davele0512 <lhduy0512@gmail.com>
2020-01-10 17:42:40 +0700

    First Heroku commit

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -111,10 +111,19 @@ WSGI_APPLICATION = 'intranet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wg_intranet_db',
+        'USERNAME': 'dave_csv',
+        'PASSWORD': 'davele0512',
     }
 }
 
