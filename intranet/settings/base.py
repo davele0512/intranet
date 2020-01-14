912fa6f81b3c8879e290bcc9cc3dde2023a7a419
davele0512 <lhduy0512@gmail.com>
2020-01-14 15:11:12 +0700

    change collectstatic to staticfiles

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -173,7 +173,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/2.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'collect_static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
